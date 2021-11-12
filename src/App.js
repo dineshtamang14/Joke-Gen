fed0dad22871e9fcb74a6b1608275deef1e82b7d
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-12 23:34:44 +0530

    complete v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
+import { useRef, useState } from "react";
 import useRandomJoke from './useRandomJoke';
 
 function App() {
    //custom hooks
-   const joke = useRandomJoke("Dinesh", "Tamang");
+   const firstNameRef = useRef(null);
+   const lastNameRef = useRef(null);
+
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const joke = useRandomJoke(firstName, lastName);
+
+   const generateJoke = e =>{
+      e.preventDefault();
+      setFirstName(firstNameRef.current.value);
+      setLastName(lastNameRef.current.value);
+   }
 
   return (
     <div className="App">
       <center>
         <h1>The Joke Generator</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+
+        <form>
+          <input placeholder="First Name" ref={firstNameRef} />
+          <input placeholder="First Name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate Joke</button>
+        </form>
+        
       </center>
     </div>
   );
