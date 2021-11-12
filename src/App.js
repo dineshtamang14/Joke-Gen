0e4fa1c5a72c70db06c4bc681138d55bdf76401a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-12 23:12:37 +0530

    added useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-import { useEffect } from "react";
+import useRandomJoke from './useRandomJoke';
 
 function App() {
-  useEffect(()=>{
-    const fetchJoke = async () => (
-      await fetch(`http://api.icndb.com/jokes/random?firstName=dinesh&lastName=tamang`)
-      .then(res => res.json())
-      .then(data => {
-      console.log(data);
-    }));
-
-    fetchJoke();
-  }, []);
+   //custom hooks
+   const joke = useRandomJoke("Dinesh", "Tamang");
 
   return (
     <div className="App">
       <center>
         <h1>The Joke Generator</h1>
-
+        <h2>{joke}</h2>
         <button>Generate Joke</button>
       </center>
     </div>
